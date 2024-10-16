4166c19eec23cf249dbcbb80c5e211082e8a2502
Federico Cáceres <fedcaceresz@gmail.com>
2024-10-16 20:24:05 -0300

    anvances en perfil.aspx

--- TPCuatrimestral_equipo_16A/Pages/Perfil.aspx
+++ TPCuatrimestral_equipo_16A/Pages/Perfil.aspx
@@ -1,5 +1,46 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Pages/Main.Master" AutoEventWireup="true" CodeBehind="Perfil.aspx.cs" Inherits="TPCuatrimestral_equipo_16A.Views.WebForm3" %>
+
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+    <div class="row g-3">
+        <div class="col-md-4">
+            <label class="form-label">Usuario</label>
+            <input type="text" class="form-control" id="user" value="" required runat="server">
+        </div>
+        <div class="col-md-4">
+            <label class="form-label">Contraseña</label>
+            <input type="password" class="form-control" id="pass" value="" required runat="server">
+        </div>
+        <div class="col-md-4">
+            <div class="form-check">
+                <input class="form-check-input" type="checkbox" id="passCheck" runat="server">
+                <label class="form-check-label">
+                    Mostrar Contraseña
+                </label>
+            </div>
+        </div>
+        <div class="col-md-4">
+            <label class="form-label">Nombre</label>
+                <input type="text" class="form-control" id="nombre" required runat="server">
+        </div>
+        <div class="col-md-4">
+            <label class="form-label">Apellido</label>
+            <input type="text" class="form-control" id="validationDefault03" required runat="server">
+        </div>
+        <div class="col-md-4">
+            <label class="form-label">Dni</label>
+            <input type="text" class="form-control" id="dni" required runat="server">
+        </div>
+        <div class="col-md-4">
+            <label class="form-label">Correo Electronico</label>
+            <input type="text" class="form-control" id="mail" required runat="server">
+        </div>
+        <div class="col-12">
+            <button class="btn btn-primary" type="button" id="btnModificar" runat="server">Modificar</button>
+            <button class="btn btn-primary" type="button" id="bntGuardar" runat="server">Guardar Cambios</button>
+        </div>
+    </div>
+
 </asp:Content>
--- TPCuatrimestral_equipo_16A/Pages/Perfil.aspx.designer.cs
+++ TPCuatrimestral_equipo_16A/Pages/Perfil.aspx.designer.cs
@@ -13,5 +13,86 @@ namespace TPCuatrimestral_equipo_16A.Views
 
     public partial class WebForm3
     {
+
+        /// <summary>
+        /// Control user.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText user;
+
+        /// <summary>
+        /// Control pass.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputPassword pass;
+
+        /// <summary>
+        /// Control passCheck.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputCheckBox passCheck;
+
+        /// <summary>
+        /// Control nombre.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText nombre;
+
+        /// <summary>
+        /// Control validationDefault03.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText validationDefault03;
+
+        /// <summary>
+        /// Control dni.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText dni;
+
+        /// <summary>
+        /// Control mail.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlInputText mail;
+
+        /// <summary>
+        /// Control bntGuardar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton bntGuardar;
+
+        /// <summary>
+        /// Control btnModificar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlButton btnModificar;
     }
 }

b5761a307c3fe5a281544cb508032582b1425c42
Franc00Baez <francobaez2309@gmail.com>
2024-10-23 21:16:51 -0300

    Creacion de entidades para el modulo de usuarios

---
+++ Domain/Entities/Operacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Operacion
+    {
+        public int Id { get; set; }
+        public string Nombre{ get; set; }
+        public Modulo Modulo { get; set; }
+
+        public Operacion() { Modulo = new Modulo(); }
+    }
+}

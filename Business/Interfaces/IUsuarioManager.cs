c001f805e756f52c8e95ffde82dfde076d4daee1
Franc00Baez <francobaez2309@gmail.com>
2024-10-26 19:46:41 -0300

    Funcion SotredProcedure para db, navegacion e IUsuarioManager

---
+++ Business/Interfaces/IUsuarioManager.cs
@@ -0,0 +1,13 @@
+﻿using Business.Managers;
+using System;
+using Domain.Entities;
+
+namespace Business.Interfaces
+{
+    public interface IUsuarioManager : ICrudRepository<Usuario>
+    {
+        Usuario ObtenerPorEmail(string email);   
+        bool VerificarPassword(string password);
+        Usuario LogIn(Usuario usuario);
+    }
+}

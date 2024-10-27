03cabf531e7b1cfcd2e1e59cd7ad82a8a21f3a58
Franc00Baez <francobaez2309@gmail.com>
2024-10-27 17:29:41 -0300

    En creacion de UsuariosMangagers /CRUD"

--- Domain/Entities/Usuario.cs
+++ Domain/Entities/Usuario.cs
@@ -15,5 +15,10 @@ namespace Domain.Entities
         public Rol Rol { get; set; }
         public string ImagenPerfil { get; set; }
         public bool Activo { get; set; }
+
+        public Usuario()
+        {
+            Rol = new Rol();
+        }
     }
 }
